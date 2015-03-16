1a1e071d5ae54ab5f182e1283af0bf40d41508d0
Kristoffer Freiholtz <Kristoffer.Freiholtz@hotmail.com>
2015-03-17 00:38:33 +0100

    Updated Input

--- src/p1/Input.java
+++ src/p1/Input.java
@@ -3,47 +3,51 @@ package p1;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.SpringLayout;
-import javax.swing.SwingConstants;
 
 public class Input extends JFrame{
-	final String[] mLabels = {"Titel", "Författare", "ISBN"};
-	
+	private final String[] mLabels = {"Titel", "Fï¿½rfattare", "ISBN"};
+	private JTextField[] mTextFields = new JTextField[mLabels.length];
 	public Input() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setPreferredSize(new Dimension(500,200));
+		setPreferredSize(new Dimension(500,150));
 		JPanel westPanel = new JPanel(new GridLayout(mLabels.length,1));
 		JPanel eastPanel = new JPanel(new GridLayout(mLabels.length,1));
 		JPanel southPanel = new JPanel(new BorderLayout());
-		JButton addButton = new JButton("Lägg Till Bok");
+		JButton addButton = new JButton("Lï¿½gg Till Bok");
+		
 		southPanel.add(addButton);
 		for (int i = 0; i < mLabels.length; i++){
 			JLabel label = new JLabel(mLabels[i]);
 			westPanel.add(label);
 			JTextField textField = new JTextField();
-			//label.setLabelFor(textField);		
+			mTextFields[i] = textField;
+			label.setLabelFor(textField);		
 			eastPanel.add(textField, BorderLayout.WEST);	
 		}
+		addButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String text = "";
+				
+				for (int i = 0; i < mLabels.length; i++){
+					text += mTextFields[i].getText() + "\n";
+				}
+				System.out.println(text);
+			}
+		});
 		eastPanel.setOpaque(true);		
 		setLayout(new BorderLayout());
 		add(westPanel, BorderLayout.WEST);
-		add(eastPanel, BorderLayout.EAST);
+		add(eastPanel, BorderLayout.CENTER);
 		add(southPanel, BorderLayout.SOUTH);
-		 
-
-		
-		//JLabel label = new JLabel("Testlabel", SwingConstants.CENTER);
-		//label.setPreferredSize(new Dimension(300, 100));
-		//getContentPane().add(label, BorderLayout.CENTER);
-		//panel.setOpaque(true);
-		//add(panel);
-		//add(panel2, BorderLayout.SOUTH);
 		setLocationRelativeTo(null);
 		pack();
 		setVisible(true);
